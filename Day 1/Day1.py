c4486b6419c714b63e9f18a818c506da95e95d9e
otacon <samuelcornelisse@gmail.com>
2021-10-24 14:21:14 +0200

    Added commentary to day 1 and 2 code.

--- Day 1/Day1.py
+++ Day 1/Day1.py
@@ -1,7 +1,13 @@
+# Open the file 'input.txt' in read mode.
 f = open("input.txt", "r");
+
+# Store the contents as a list of the lines of the file.
 contents = f.readlines();
+
+# Add up the number in each line into one variable.
 a = 0;
 for line in contents:
     a += int(line);
 
+# Print the final result.
 print(a);
